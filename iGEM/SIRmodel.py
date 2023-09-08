353e0bd823a767c50d95c7eb5a84817adfc3fb02
w12l3-c <wallace.lee0412@gmail.com>
2023-09-07 22:15:13 -0400

    Ode + Monte

--- iGEM/SIRmodel.py
+++ iGEM/SIRmodel.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 from scipy.integrate import odeint, solve_ivp
 from scipy.optimize import minimize
 from tqdm import tqdm
-import math
-from ETImodel import ETImodel
+from ETImodel import PlantETI
 
 class SIRmodel:
     def __init__(self, N, I0, R0, S0, Rn, beta, gamma, days):
@@ -23,10 +23,13 @@ class SIRmodel:
         self.days = days
         
         self.t_span = (0, days) # (start, max days)
-        self.t_eval = [x for x in range(days+1)]
+        self.t_eval = [x for x in range(days)]
         self.t = np.linspace(0, days, days) # (0, amount of days, steps)
         self.endtime = days
         self.y0 = S0, I0, R0
+
+        self.fig, self.ax = plt.subplots()
+        self.lines = []
 
         if self.N >= 1e8:
             self.ratio = 1e6
@@ -78,7 +81,8 @@ class SIRmodel:
         plt.show(block=True)
 
     ## Monte Carlo Simulation
-    def monteCarlo(self, num_simulations = 9):
+    # Monte Carlo Simulation by my own logic
+    def monteCarlo(self, num_simulations=9):
         self.results = []
         self.hyperparameters = []
         self.num_simulations = num_simulations
@@ -94,7 +98,7 @@ class SIRmodel:
         
             # threshold = 8
 
-            for day in tqdm(range(self.days)):
+            for day in range(self.days):
                 new_infected = np.random.binomial(S[-1], self.beta * I[-1] / self.N)
                 # if new_infected > threshold:
                 #     new_infected = threshold
@@ -110,27 +114,51 @@ class SIRmodel:
             self.results.append((S, I, R, t))
             self.hyperparameters.append((self.beta, self.gamma))
 
+    # Monte Carlo Simulation with ODE
+    def monteCarlo2(self, num_sim=100):
+        self.results = []
+        self.hyperparameters = []
+        self.num_simulations = num_sim
+        for _ in tqdm(range(num_sim)):
+            # Randomness Event
+            self.beta = np.random.uniform(0.5, 1.5)
+            self.gamma = np.random.uniform(0, 0.2)
+
+            self.hyperparameters.append((self.beta, self.gamma))
+
+            self.y0 = self.S0, self.I0, self.R0
+            S, I, R = odeint(self.deriv, self.y0, self.t, args=(self.N, self.beta, self.gamma)).T
+            self.results.append((S, I, R, np.array(self.t_eval)))
+
     def plotMonteCarlo(self):
-        fig, ax = plt.subplots(3, 3, figsize=(15, 8))
-        if self.num_simulations > 9:
-            fig, ax = plt.subplots(int(math.ceil(self.num_simulations//3)), 3, figsize=(15, 8))
+        self.results = np.array(self.results)
+        self.hyperparameters = np.array(self.hyperparameters)
+
+        self.mean_results = np.mean(self.results, axis=0)
+        self.std_results = np.std(self.results, axis=0)
+
+        self.mean_hyperparameters = np.mean(self.hyperparameters, axis=0)
+        self.std_hyperparameters = np.std(self.hyperparameters, axis=0)
+
+        S, I, R, t = self.mean_results
+        beta, gamma = self.mean_hyperparameters
+        y = np.vstack([I, S, R])
+        labels = ['Infected', 'Suspectible', 'Recovered']
+        color_map = ["#db1d0f", "#0e85ed", "#19e653"]
+        plt.title(f'Monte Carlo Sim for TSWV with {self.num_simulations} simulations | beta:{beta:.2f} | gamma:{gamma:.4f}')
+        plt.stackplot(t, y, labels=labels, colors=color_map)
+        plt.xlabel('Time')
+        plt.ylabel(f'Population')
+        plt.legend(loc='upper left')
         
-        for i, result in enumerate(self.results):
-            S, I, R, t = result
-            beta, gamma = self.hyperparameters[i]
-            y = np.vstack([I, S, R])
-            labels = ['Infected', 'Suspectible', 'Recovered']
-            color_map = ["#db1d0f", "#0e85ed", "#19e653"]
-            j = i // 3
-            k = i % 3
-            ax[j, k].set_title(f'beta:{beta:.2f} | gamma:{gamma:.4f}')
-            ax[j, k].stackplot(t, y, labels=labels, colors=color_map)
-            fig.suptitle(f'Monte Carlo Sim for TSWV')
-            ax[j, k].set_xlabel('Time in days')
-            ax[j, k].set_ylabel(f'Population')
-            # ax[0].legend(loc='upper left')
-        plt.subplots_adjust(hspace=0.5)
-        plt.show(block=True)
+        self.ani = FuncAnimation(self.fig, self.update_plot, frames=len(t), blit=True)
+        plt.show(blocj=True)
+
+    def update_plot(self, frame):
+        for i, line in enumerate(self.lines):
+            line.set_data(self.mean_results[3][:frame], self.mean_results[i][:frame])
+        return self.lines
+
             
 # I'll add PTI and ETI later as it is correlated with the iRNA and lab
 
@@ -149,20 +177,20 @@ if __name__ == '__main__':
     # Although technically best Rt = S*Rn*e^(Rn*(gamma*(N-R)*t - gamma^2*I*(t^2/2) + (1/Rn)*(S/I)*(e^(-gamma*Rn*I*t - 1)))))
     # But the solve_ivp will deal with that for us and iterate Rt
 
-    print("How many trails do you want to run?")
-    trails = int(input("Enter a number:"))
+    # print("How many trails do you want to run?")
+    # trails = int(input("Enter a number:"))
 
-    for i in tqdm(range(trails)):
-        TSWV = SIRmodel(N=N, I0=I0, R0=R0, S0=S0, Rn=Rn, beta=beta, gamma=gamma, days=80)
-        TSWV.ode()
-        TSWV.plotSIR()
+    # for i in tqdm(range(trails)):
+    #     TSWV = SIRmodel(N=N, I0=I0, R0=R0, S0=S0, Rn=Rn, beta=beta, gamma=gamma, days=80)
+    #     TSWV.ode()
+    #     TSWV.plotSIR()
 
-        beta += 0.1
-        gamma /= 10
+    #     beta += 0.1
+    #     gamma /= 10
     
     print("How many sims do you want to run (default 9)?")
     trails = int(input("Enter a number:"))
 
-    TSWV = SIRmodel(N=N, I0=I0, R0=R0, S0=S0, Rn=Rn, beta=0.4, gamma=0.05, days=80)
-    TSWV.monteCarlo(trails)
+    TSWV = SIRmodel(N=N, I0=I0, R0=R0, S0=S0, Rn=Rn, beta=0.4, gamma=0.05, days=150)
+    TSWV.monteCarlo2(trails)
     TSWV.plotMonteCarlo()
