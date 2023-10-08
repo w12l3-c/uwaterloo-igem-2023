b1433616d7568475f3993721366ee0775321f842
w12l3-c <wallace.lee0412@gmail.com>
2023-10-08 17:53:01 -0400

    Fix something in interference

--- iGEM/Interference2_ODE.py
+++ iGEM/Interference2_ODE.py
@@ -13,26 +13,24 @@ risc = 0
 tswv = 100
 risc_tswv = 0
 
-time = 10
+time = 500
 time_step = 1
-day = 0
+min = 0
 
 limit = 1e10
 limit_tswv = 1e10
 
-start_infection = 5 # RISC will only start increasing after virus infection
+start_infection = 0 # RISC will only start increasing after virus infection
 
 # Rates & Percentages — Currently all made up values
-ecoli_replicate_rate = np.log(2)/20*60*24   # double every 20 minutes convert to day
-ecoli_degrade_rate = np.log(2)/6*60*24    # half life of 6 minutes convert to day
+ecoli_replicate_rate = np.log(2)/20   # double every 20 minutes convert to day
+ecoli_degrade_rate = np.log(2)/6    # half life of 6 minutes convert to day
 ecoli2dsRNA_percent = 0.05
 dsRNA2siRNA_percent = 0.95
 siRNA2risc_percent = 1.0 
-tswv_replicate_rate = np.random.uniform(0.1, 1) * 60 * 24
+tswv_replicate_rate = np.random.uniform(0.1, 1)
+tswv_replicate_rate = 1.2
 risc_tswv_degrade_percent = 0.0000001
-
-tswv_cycle = 20
-tswv_multiplier = 6000 # Average Range Negative Strand RNA burst size
 
 # Stacks
 ecoli_stack = []
@@ -44,29 +42,6 @@ tswv_stack = []
 risc_tswv_stack = []
 
 # ==================== ODE ==================== #
-
-# Probability of RISC inferencing TSWV
-# def risc_tswv_prob():
-#     return random.uniform(0,1)
-
-# Monte Carlo between TSWV and RISC
-# def model_risc_tswv(risc, tswv):
-#     tswv_change = 0
-#     max_interaction = min(risc, tswv)
-
-#     for i in range(max_interaction):
-#         if random.uniform(0,1) < risc_tswv_prob():
-#             tswv_change -= 1
-
-#     return tswv_change
-
-# Virus duplicate
-# def model_tswv(tswv, t):
-#     tswv_multiplier = random.randint(3000, 6000)
-#     tswv_multipler = tswv_multiplier * (np.sin(2 * np.pi * t / tswv_cycle))
-#     tswv += tswv * 0.2 * tswv_multipler # 0.2 percent of tswv replicate
-#     return tswv
-
 # Stopping
 def stop(t, y, e_g, e_d, d_g, d_d, s_d, t_g, rt):
     return tswv > 1e15 or tswv < 1e-15
@@ -77,19 +52,20 @@ def interference_ode(t, y, e_g, e_d, d_g, d_d, s_d, t_g, rt):
     ecoli, dsRNA, siRNA, risc, tswv = y
 
     d_ecoli = ((e_g) * ecoli) * (1 - (ecoli/limit))     # Logistic Growth
-    # d_minicell = e_d * ecoli * - m_d * minicell           # minicell touches plant cell it degrades instantly
     d_dsRNA = 500 * d_g * ecoli - d_d * dsRNA             # optimally 500 plasmid per minicell
     d_siRNA = 20 * d_d * dsRNA - s_d * siRNA              # 20 sites in dsRNA to become siRNA
-    d_risc = s_d * siRNA                                  # 1 siRNA = 1 RISC - RISC bind with TSWV
+    d_risc = s_d * siRNA                                  # 1 siRNA = 1 RISC 
 
     if t > start_infection:
-        half_life = np.log(2)/6.395 * 24 * 60             # half life of 6.395 minutes for mRNA
-        diff = tswv - risc
-        if diff > 0:
-            rt = risc
+        half_life = np.log(2)/6.395             # half life of 6.395 minutes for mRNA
+        diff = tswv - risc                      # difference between tswv and risc
+        
+        # if tswv more than risc
+        if diff > 0:                            
+            d_tswv = - (risc)
         else:
-            rt = tswv
-        d_tswv = (t_g * tswv - half_life * tswv - rt) * (1 - (tswv/limit_tswv))
+            tswv_2_risc = tswv/risc
+            d_tswv = (t_g * tswv - half_life * tswv - risc * tswv_2_risc * 1.001) #* (1 - (tswv/limit_tswv))
     else:
         d_tswv = 0     
     
@@ -100,23 +76,28 @@ y0 = [ecoli, dsRNA, siRNA, risc, tswv]
 t_span = (0, time)
 rates = (ecoli_replicate_rate, ecoli_degrade_rate, ecoli2dsRNA_percent, dsRNA2siRNA_percent, siRNA2risc_percent, tswv_replicate_rate, risc_tswv_degrade_percent)
 
-solution = solve_ivp(interference_ode, t_span, y0, args=(rates), dense_output=False)
+solution = solve_ivp(interference_ode, t_span, y0, args=(rates), dense_output=False, rtol=1e-3, atol=1e-6)
 
 # Plot results
 fig, ax = plt.subplots(3, 2, figsize=(20, 8))
 fig.tight_layout(pad=4.0)
-ax[0, 0].plot(solution.t, solution.y[0], label="Ecoli")
+ax[0, 0].plot(solution.t, solution.y[0], label="Ecoli", color='blue')
 ax[0, 0].set_title('Ecoli')
-ax[0, 1].plot(solution.t, solution.y[1], label="dsRNA")
+ax[0, 1].plot(solution.t, solution.y[1], label="dsRNA", color='orange')
 ax[0, 1].set_title('dsRNA')
-ax[1, 0].plot(solution.t, solution.y[2], label="siRNA")
+ax[1, 0].plot(solution.t, solution.y[2], label="siRNA", color='green')
 ax[1, 0].set_title('siRNA')
-ax[1, 1].plot(solution.t, solution.y[3], label="RISC")
+ax[1, 1].plot(solution.t, solution.y[3], label="RISC", color='red')
 ax[1, 1].set_title('RISC')
-ax[2, 0].plot(solution.t, solution.y[4], label="TSWV")
+ax[2, 0].plot(solution.t, solution.y[4], label="TSWV", color='purple')
 ax[2, 0].set_title('TSWV')
-# ax[2, 1].plot(solution.t, solution.y[5], label="RISC-TSWV")
-# ax[2, 1].set_title('RISC-TSWV')
+ax[2, 1].plot(solution.t, solution.y[0], label="Ecoli")
+ax[2, 1].plot(solution.t, solution.y[1], label="dsRNA")
+ax[2, 1].plot(solution.t, solution.y[2], label="siRNA")
+ax[2, 1].plot(solution.t, solution.y[3], label="RISC")
+ax[2, 1].plot(solution.t, solution.y[4], label="TSWV")
+ax[2, 1].set_title('Everything')
+ax[2, 1].legend(loc="upper left")
 fig.suptitle('mRNA Interference')
 plt.show()
 
